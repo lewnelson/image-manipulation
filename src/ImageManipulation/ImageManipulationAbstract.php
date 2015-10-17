24e87365efec65540a615928424fa76c216d1ff6
lewnelson <lewis@lewnelson.com>
2015-10-17 10:35:29 +0100

    Modified package name

--- src/ImageManipulation/ImageManipulationAbstract.php
+++ src/ImageManipulation/ImageManipulationAbstract.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the LewisNelson/ImageManipulation package.
+ * This file is part of the lewnelson/image-manipulation package.
  *
  * (c) Lewis Nelson <lewis@lewnelson.com>
  *
